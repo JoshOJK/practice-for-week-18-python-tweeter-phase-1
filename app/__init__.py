d3759e8eac72c570325175551715bd286b3684cc
joshjk <jokrienke@comcast.net>
2023-08-23 22:05:44 -0700

    phase 2 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,19 @@
 # !!START
-from flask import Flask
+from flask import Flask, render_template
 from .config import Config
+from .tweets import tweets
+
+import random
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
 # !!END
+
+@app.route('/')
+def home():
+    tweet = random.choice(tweets)
+    return render_template('index.html', tweet=tweet)
+
+# app.route('/feed')
+# def feed():
