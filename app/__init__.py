775246bb000aa4f66624f1d39fc02c36b151b534
joshjk <jokrienke@comcast.net>
2023-08-24 20:12:59 -0700

    completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
 # !!START
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .config import Config
 from .tweets import tweets
-
+from .form.form import TweetForm
 import random
+from datetime import date
 
 app = Flask(__name__)
 
@@ -15,5 +16,28 @@ def home():
     tweet = random.choice(tweets)
     return render_template('index.html', tweet=tweet)
 
-# app.route('/feed')
-# def feed():
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', tweets=tweets)
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    form = TweetForm()
+
+    if form.validate_on_submit():
+        new_tweet={
+            "id": len(tweets),
+            "author": form.data['author'],
+            "tweet": form.data['tweet'],
+            "date": date.today(),
+            "likes": 0
+        }
+
+        tweets.append(new_tweet)
+        return redirect('/feed', 302)
+
+    if form.errors:
+        return form.errors
+
+
+    return render_template("new_tweet.html", form=form)
